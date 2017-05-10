5474b698955d2bcde670b2f17cdaf003bb103f92
Paul Watson <paul.watson@pdwtech.com>
2017-05-10 11:27:26 +0100

    Help, Stop and Cancel Intents

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS \
-          -Dgoogle_creds="$google_creds" \
-          -Dcom.amazon.speech.speechlet.servlet.supportedApplicationIds=$ALEXA_APPLICATION_ID
-          -javaagent:build/lib/newrelic.jar \
-          -jar build/libs/cask-alexa.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS  -Dgoogle_creds="$google_creds"  -Dcom.amazon.speech.speechlet.servlet.supportedApplicationIds=$ALEXA_APPLICATION_ID  -javaagent:build/lib/newrelic.jar  -jar build/libs/cask-alexa.jar
--- src/main/java/com/caskalexa/service/CaskSpeechlet.java
+++ src/main/java/com/caskalexa/service/CaskSpeechlet.java
@@ -15,6 +15,10 @@ public class CaskSpeechlet implements Speechlet {
 
     public static final String INTRO = "Welcome to Cask Beers. ";
     public static final String HOW_TO = "Find out more about drinks by asking: what is on?";
+    public static final String AMAZON_HELP_INTENT = "AMAZON.HelpIntent";
+    public static final String AMAZON_STOP_INTENT = "AMAZON.StopIntent";
+    public static final String AMAZON_CANCEL_INTENT = "AMAZON.CancelIntent";
+    public static final String HELP_SPEECH = "Cask Beers can tell you more information about the daily beer menu. You can cask for me info by asking: What is on the menu?";
 
     @Autowired
     MenuService menuService;
@@ -50,10 +54,7 @@ public class CaskSpeechlet implements Speechlet {
             try {
                 beersListSpeech = menuService.topBeersSpeech();
             } catch (IOException e) {
-                PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-                speech.setText("I'm sorry, I'm having trouble reading the menu!");
-                SpeechletResponse problemResponse = SpeechletResponse.newTellResponse(speech);
-                return problemResponse;
+                return respond("I'm sorry, I'm having trouble reading the menu!");
             }
 
             SimpleCard card = new SimpleCard();
@@ -65,6 +66,10 @@ public class CaskSpeechlet implements Speechlet {
 
             SpeechletResponse response = SpeechletResponse.newTellResponse(speech, card);
             return response;
+        } else if (intentName.equals(AMAZON_HELP_INTENT)){
+            return ask(HELP_SPEECH);
+        } else if (intentName.equals(AMAZON_CANCEL_INTENT) || intentName.equals(AMAZON_STOP_INTENT)) {
+            return new SpeechletResponse();
         }
         else {
             throw new SpeechletException("I don't understand that intent.");
@@ -74,5 +79,17 @@ public class CaskSpeechlet implements Speechlet {
     @Override
     public void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException {
 
+    }
+
+    private SpeechletResponse respond(String text) {
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(text);
+        return SpeechletResponse.newTellResponse(speech);
+    }
+
+    private SpeechletResponse ask(String text) {
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(text);
+        return SpeechletResponse.newTellResponse(speech);
     }
 }
